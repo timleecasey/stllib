f0da37bb52393442a9a6f8971d598d633d4466c2
Tim Casey <tlc@veeve.io>
2025-06-24 10:08:26 -0700

    Maybe a stiffness as well, for deflection place holder.

--- lib/aid3/sim/tooling/material.go
+++ lib/aid3/sim/tooling/material.go
@@ -2,23 +2,30 @@ package tooling
 
 type Material interface {
 	Hardness() float64
+	Stiffness() float64
 	Volume() Volume
 }
 
 type Wood struct {
 	hardness    float64
+	stiffness   float64
 	startVolume Volume
 }
 
 func MakeWood(dim float64) Material {
 	return &Wood{
 		hardness:    0.0,
+		stiffness:   0.0,
 		startVolume: MakeVolume(&Point{-dim, -dim, -dim}, &Point{dim, dim, dim}),
 	}
 }
 
 func (w *Wood) Hardness() float64 {
 	return w.hardness
+}
+
+func (w *Wood) Stiffness() float64 {
+	return w.stiffness
 }
 
 func (w *Wood) Volume() Volume {
